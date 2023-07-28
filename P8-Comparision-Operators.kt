ecdf8d8586fb8c15dc69786102802d62466882e1
Nishanth Reddy <nishanthreddy179@gmail.com>
2023-07-28 11:49:27 +0530

    Program08 Add: Comparison operators

---
+++ P8-Comparision-Operators.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val x = 5
+    val y = 10
+
+    // the following code compares the variables and returns a boolean value
+
+    println("x = y: ${x == y}")          // compares if x is equal to y
+    println("x != y: ${x != y}")         // compares if x is not equal to y
+    println("x > y: ${x > y}")           // compares if x is greater than y
+    println("x < y: ${x < y}")           // compares if x is less than y
+    println("x >= y: ${x >= y}")         // compares if x is greater than y
+    println("x <= y: ${x <= y}")         // compares if x is less than y
+}
